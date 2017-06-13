b5627e12d05c73d48c0bdb6433d2f9e594128559
Ronan Merien <ronan.merien@gmail.com>
2017-06-13 14:22:09 +0200

    notify

--- app/src/main/java/com/example/admin/tp9android/MainActivity.java
+++ app/src/main/java/com/example/admin/tp9android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.admin.tp9android;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast toast = Toast.makeText(this, R.string.create, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast toast = Toast.makeText(this, R.string.pause, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast toast = Toast.makeText(this, R.string.resume, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast toast = Toast.makeText(this, R.string.start, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        notify("onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onRestoreInstanceState(outState);
+        notify("onSaveInstanceState");
+    }
+
+    private void notify(String methodName) {
+        String name = this.getClass().getName();
+        String[] strings = name.split("\\.");
+        Notification noti = new Notification.Builder(this)
+                .setContentTitle(methodName + " " + strings[strings.length - 1]).setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText(name).build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify((int) System.currentTimeMillis(), noti);
     }
 }
---
+++ app/src/main/java/com/example/admin/tp9android/Tracker.java
@@ -0,0 +1,15 @@
+package com.example.admin.tp9android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Tracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracker);
+    }
+}
